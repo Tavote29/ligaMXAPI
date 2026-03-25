bcd827cdd70955ac5ba203926c7236d2d68ad843
Tavote29 <gustavo_angel2000@hotmail.com>
2026-03-25 17:50:02 -0600

    creacion de repositorio

---
+++ src/main/java/com/fdf/liga_mx/LigaMxApplication.java
@@ -0,0 +1,13 @@
+package com.fdf.liga_mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaMxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigaMxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdf/liga_mx/LigaMxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdf.liga_mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaMxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
